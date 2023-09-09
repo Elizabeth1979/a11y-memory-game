df6245abacca053fe52e9ddeaadc54881c64d7c3
elli-patrick <elizabethp@wix.com>
2023-09-09 10:36:27 +0300

    timer display

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -2,53 +2,28 @@ import { useEffect, useState } from "react";
 import "./Timer.css";
 
 function Timer({ isGameOver, isResetTimer }) {
-  // const [seconds, setSeconds] = useState(0);
-  // const [minutes, setMinutes] = useState(0);
-  const [_time, setTime] = useState(0);
+  const [time, setTime] = useState(0);
 
-  // const timeRepresentation = (timeFraction) => {
-  //   return timeFraction < 10 ? `0${timeFraction}` : `${timeFraction}`;
-  // };
-
-  // const time = `${timeRepresentation(minutes)}:${timeRepresentation(seconds)}`;
-
-  // useEffect(() => {
-  //   if (isResetTimer) {
-  //     setMinutes(0);
-  //     setSeconds(0);
-  //   }
-  // }, [isResetTimer]);
+  const timeRepresentation = (timeFraction) => {
+    return timeFraction < 10 ? `0${timeFraction}` : `${timeFraction}`;
+  };
 
   useEffect(() => {
-    if(isGameOver)return;
-    
+    if (isGameOver) return;
+
     const timeInterval = setInterval(() => {
-      setTime(_time + 1);
-      // setSeconds((prevSeconds) => prevSeconds + 1);
-      // if (seconds === 59) {
-      //   setSeconds(0);
-      //   setMinutes((prevMinutes) => prevMinutes + 1);
-      // }
+      setTime(time + 1);
     }, 1000);
 
     return () => {
-      console.log('cleanup');
       clearInterval(timeInterval);
-    }
+    };
   });
 
   return (
     <>
       <p className="visually-hidden">Timer</p>
-      {Math.floor((_time / 60).toFixed(0))}:{_time % 60}
-
-      {/* {isGameOver ? (
-        <div className="timer-container">
-          {_time / 60}:{_time % 60}
-        </div>
-      ) : (
-        <div className="timer-container">{time}</div>
-      )} */}
+      {timeRepresentation(Math.floor((time / 60).toFixed(0)))}:{timeRepresentation(time % 60)}
     </>
   );
 }

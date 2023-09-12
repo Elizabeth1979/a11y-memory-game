d6a5b32e33fcdd7ce93511b37828f4a6cb887697
elli-patrick <elizabethp@wix.com>
2023-09-12 16:18:09 +0300

    card animation finalize

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ function Card({ card, handleChoice, flipped }) {
   const cardRef = useRef(null);
 
   const handleClick = () => {
-      handleChoice(card);
+    handleChoice(card);
 
     setTimeout(() => {
       cardRef.current.focus();
@@ -14,24 +14,26 @@ function Card({ card, handleChoice, flipped }) {
   };
 
   return (
-    <li className={`card ${flipped ? "flipped" : ""}`}>
-      <div className="back">
-        <button
-          aria-label={`back of card ${card.cardNumber}`}
-          className="card-button"
-          onClick={handleClick}
-        ></button>
-      </div>
-      <div
-        ref={cardRef}
-        role="group"
-        aria-labelledby={card.id}
-        tabIndex="-1"
-        className={`front ${card.match ? "correct" : "wrong"}`}
-      >
-        <div id={card.id}>
-          <p className="card-type">{card.type}</p>
-          <p className="card-description">{parse(card.description)}</p>
+    <li className="card">
+      <div className={`card-inner ${flipped ? "flipped" : ""}`}>
+        <div className="back">
+          <button
+            aria-label={`back of card ${card.cardNumber}`}
+            className="card-button"
+            onClick={handleClick}
+          ></button>
+        </div>
+        <div
+          ref={cardRef}
+          role="group"
+          aria-labelledby={card.id}
+          tabIndex="-1"
+          className={`front ${card.match ? "correct" : "wrong"}`}
+        >
+          <div id={card.id}>
+            <p className="card-type">{card.type}</p>
+            <p className="card-description">{parse(card.description)}</p>
+          </div>
         </div>
       </div>
     </li>
